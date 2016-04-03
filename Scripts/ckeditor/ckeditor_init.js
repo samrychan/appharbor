7b7935fde4b313f6453a3d1feae09b1fccd14a7b
samrychan <javast@qq.com>
2016-04-03 15:12:17 +0800

    yaf

---
+++ Scripts/ckeditor/ckeditor_init.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function() {
+    var yafCKEditor = jQuery('textarea.YafTextEditor').ckeditor({
+        extraPlugins: 'autosave,bbcodehtml,syntaxhighlight,bbcodeselector,codemirror,textselection,wordcount',
+        autosave_saveDetectionSelectors: "a[id*='_PostReply'],a[id*='Cancel']",
+        toolbar: [
+            ['Source'],
+            ['Cut', 'Copy', 'Paste'], ['Undo', 'Redo', '-', 'Find', 'Replace', '-', 'SelectAll', 'RemoveFormat'],
+            ['-', 'NumberedList', 'BulletedList'],
+            ['-', 'Link', 'Unlink', 'Image'],
+            ['Blockquote', 'syntaxhighlight', 'bbcodeselector'],
+            ['SelectAll', 'RemoveFormat'],
+            ['About'],
+            '/',
+            ['Bold', 'Italic', 'Underline', '-', 'TextColor', 'Font', 'FontSize'],
+            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'PasteText', 'PasteFromWord'],
+            ['Outdent', 'Indent']
+        ],
+        entities_greek: false,
+        entities_latin: false,
+        language: editorLanguage,
+        contentsCss: 'Scripts/ckeditor/yaf_contents.css',
+        codemirror:
+        {
+            mode: 'bbcodemixed'
+        }
+    });
+
+    jQuery("a[id*='_PostReply'],a[id*='_Save']").click(function () {
+        yafCKEditor.editor.updateElement();
+    });
+
+    yafCKEditor.editor.addCommand('highlight', {
+        modes: { wysiwyg: 1, source: 1 },
+        exec: function (editor) {
+            var selection = editor.getSelection();
+            if (!selection) {
+                editor.insertHtml('[h]' + '[/h]');
+            }
+            var text = selection.getSelectedText();
+
+            editor.insertHtml('[h]' + text + '[/h]');
+        }
+    });
+
+    yafCKEditor.editor.addCommand('postmessage', {
+        modes: { wysiwyg: 1, source: 1 },
+        exec: function () {
+            if (jQuery("a[id*='_PostReply']").length) {
+                __doPostBack(jQuery("a[id*='_PostReply']").attr('id').replace('_', '$').replace('_', '$'), '');
+            } else if (jQuery("a[id*='_Save']").length) {
+                __doPostBack(jQuery("a[id*='_Save']").attr('id').replace('_', '$').replace('_', '$'), '');
+            }
+        }
+    });
+});
---
+++ install/custom/dnn.sql
@@ -0,0 +1,93 @@
+﻿/*
+** DNN Custom SQL Procedures
+*/
+
+IF  EXISTS (select top 1 1 from sys.objects WHERE object_id = OBJECT_ID(N'[{databaseOwner}].[{objectQualifier}YafDnn_Messages]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [{databaseOwner}].[{objectQualifier}YafDnn_Messages]
+GO
+
+IF  EXISTS (select top 1 1 from sys.objects WHERE object_id = OBJECT_ID(N'[{databaseOwner}].[{objectQualifier}YafDnn_Topics]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [{databaseOwner}].[{objectQualifier}YafDnn_Topics]
+GO
+
+IF  EXISTS (select top 1 1 from sys.objects WHERE object_id = OBJECT_ID(N'[{databaseOwner}].[{objectQualifier}GetReadAccessListForForum]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [{databaseOwner}].[{objectQualifier}GetReadAccessListForForum]
+GO
+
+/** Create Stored Procedures **/
+
+CREATE PROCEDURE [{databaseOwner}].[{objectQualifier}YafDnn_Messages]
+	AS
+	SET NOCOUNT ON
+	SELECT
+		Message, MessageID, TopicID, Posted
+	FROM [{databaseOwner}].[{objectQualifier}Message]
+GO
+
+CREATE PROCEDURE [{databaseOwner}].[{objectQualifier}YafDnn_Topics]
+	AS
+	SET NOCOUNT ON
+	SELECT
+		Topic, TopicID, ForumID, Posted
+	FROM [{databaseOwner}].[{objectQualifier}Topic]
+
+GO
+
+CREATE PROCEDURE [{databaseOwner}].[{objectQualifier}GetReadAccessListForForum](
+  @ForumID int)
+	AS
+	select fa.GroupID, GroupName = g.Name, AccessMaskName = am.Name, am.Flags
+	from [{databaseOwner}].[{objectQualifier}ForumAccess] fa
+	Inner join [{databaseOwner}].[{objectQualifier}AccessMask] am
+    on (fa.AccessMaskID = am.AccessMaskID)
+	inner join [{databaseOwner}].[{objectQualifier}Group] g
+	on (fa.GroupID = g.GroupID)
+	where fa.ForumID = @ForumID
+GO
+
+
+IF  EXISTS (select top 1 1 from sys.objects WHERE object_id = OBJECT_ID(N'[{databaseOwner}].[RemoveUser]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [{databaseOwner}].[RemoveUser]
+GO
+
+
+CREATE PROCEDURE [{databaseOwner}].[RemoveUser]
+	@UserID		int,
+	@PortalID   int
+AS
+    DECLARE @Email  nvarchar(255)
+	DECLARE @YafUserID int
+
+	SELECT @Email = Email FROM [{databaseOwner}].[Users] WHERE UserId = @UserID
+
+	IF @PortalID IS NULL
+		BEGIN
+			-- Delete SuperUser
+			DELETE FROM [{databaseOwner}].[Users]
+				WHERE  UserId = @UserID
+		END
+	ELSE
+		BEGIN
+			-- Remove User from Portal			
+			DELETE FROM [{databaseOwner}].[UserPortals]
+				WHERE  UserId = @UserID
+                 AND PortalId = @PortalID
+			IF NOT EXISTS (SELECT 1 FROM [{databaseOwner}].[UserPortals] WHERE  UserId = @UserID) 
+				-- Delete User (but not if SuperUser)
+				BEGIN
+					DELETE FROM [{databaseOwner}].[Users]
+						WHERE  UserId = @UserID
+							AND IsSuperUser = 0
+					DELETE FROM [{databaseOwner}].[UserRoles]
+						WHERE  UserID = @UserID
+				END								
+		END
+
+-- Delete user in YAF.NET
+SELECT @YafUserID = UserID FROM [{databaseOwner}].[{objectQualifier}User] WHERE Email = @Email
+				   
+DELETE FROM [{databaseOwner}].[{objectQualifier}UserAlbum] WHERE UserID = @YafUserID
+			   
+EXEC [{databaseOwner}].[{objectQualifier}user_delete] @YafUserID
+
+GO
